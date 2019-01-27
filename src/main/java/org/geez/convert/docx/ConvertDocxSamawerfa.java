da03a230de175792de26acf284d7d16ef0dbf339
dyacob <yacob@geez.org>
2019-01-27 08:00:57 -0500

    Adding Tigrinya ቐ series

--- src/main/java/org/geez/convert/docx/ConvertDocxSamawerfa.java
+++ src/main/java/org/geez/convert/docx/ConvertDocxSamawerfa.java
@@ -18,27 +18,8 @@ public class ConvertDocxSamawerfa extends ConvertDocxDiacriticalSystem {
 	public ConvertDocxSamawerfa() {
 		this.initialize( "Samawerfa.txt", "Samawerfa.txt", "Addis98", "Addis98" );
 		huletNeteb = '\u003a';
-		
-		font1Typefaces.add( "Addis98" );
-		font1Typefaces.add( "Addis98w" );
-		font1Typefaces.add( "AddisWP" );
-		font1Typefaces.add( "Addis98 regular" );
-		font1Typefaces.add( "Addis98w regular" );
-		font1Typefaces.add( "AddisWP regular" );
-		
-		targetTypefaces.add( "Addis98" );
-		targetTypefaces.add( "Addis98w" );
-		targetTypefaces.add( "AddisWP" );
-		targetTypefaces.add( "Addis98 regular" );
-		targetTypefaces.add( "Addis98w regular" );
-		targetTypefaces.add( "AddisWP regular" );
-		
-		for(String key: font1Typefaces) {
-			fontToTransliteratorMap.put( key, translit1 );			
-		}
 
 		translit2 = null;
-
 
 		diacritics.addAll (
 				Arrays.asList( "\u00fd", "\u00e7", "\u00e6", "\u00e5", "\u00e8", "\u00e9", "\u00ea", "\u00eb", "\u00ec", "\u00ed", "\u00ee", "\u00ef", "\u00f0", "\u00f1", "\u00f2", "\u00f9", "\u00f8", "\u00f7", "\u00f6" ,"\u00f5", "\u00f4", "\u00f3", "\u00fb", "\u00fc", "\u00fa", "\u003a" )
