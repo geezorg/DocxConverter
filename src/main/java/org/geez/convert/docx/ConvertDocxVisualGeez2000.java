94840e71ef5c36afafb0f29a9ad6e2c35f9438d3
dyacob <yacob@geez.org>
2019-02-24 00:38:19 -0500

    corrected diacritics list in VG2000

--- src/main/java/org/geez/convert/docx/ConvertDocxVisualGeez2000.java
+++ src/main/java/org/geez/convert/docx/ConvertDocxVisualGeez2000.java
@@ -43,7 +43,7 @@ public class ConvertDocxVisualGeez2000 extends ConvertDocxDiacriticalSystem {
 
 
 		diacritics.addAll (
-			Arrays.asList( "\u0026", "\u002a", "\u0040", "\u0045", "\u00a4", "\u00b7", "\u00bd", "\u00d6", "\u00dc", "\u00f9", "\u00fe", "\u201c" )
+			Arrays.asList( "\u0026", "\u002a", "\u0045", "\u00a4", "\u00b7", "\u00bd", "\u00d6", "\u00dc", "\u00f9", "\u00fe", "\u201c" )
 		);
 				
 		buildRE();
