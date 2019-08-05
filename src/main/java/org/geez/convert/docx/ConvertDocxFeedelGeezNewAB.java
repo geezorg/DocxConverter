5ca3306a2b184283db06bc9989604c8066ef95a2
dyacob <yacob@geez.org>
2019-08-05 09:32:52 -0400

    adding ids for converters

--- src/main/java/org/geez/convert/docx/ConvertDocxFeedelGeezNewAB.java
+++ src/main/java/org/geez/convert/docx/ConvertDocxFeedelGeezNewAB.java
@@ -7,17 +7,14 @@ import java.util.List;
 
 import org.docx4j.wml.Text;
 
-/*
- * The non-maven way to build the jar file:
- *
- * javac -Xlint:deprecation -cp docx4j-6.0.1.jar:dependencies/commons-io-2.5.jar:../icu4j-63_1.jar:dependencies/slf4j-api-1.7.25.jar:slf4j-1.7.25 *.java
- * jar -cvf convert.jar org/geez/convert/docx/*.class org/geez/convert/tables/
- * java -cp convert.jar:docx4j-6.0.1.jar:dependencies/*:../icu4j-63_1.jar:slf4j-1.7.25/slf4j-nop-1.7.25.jar org.geez.convert.docx.ConvertDocx geeznewab myFile-In.docx myFile-Out.docx
- *
- */
 
 
 public class ConvertDocxFeedelGeezNewAB extends ConvertDocxDiacriticalSystem {
+
+	{
+		IDs = new String[] { "GeezNewA", "GeezNewB" } ;
+	}
+	
 	private final List<String> font2Typefaces = new ArrayList<String>();
 
 	public ConvertDocxFeedelGeezNewAB( final File inputFile, final File outputFile ) {
